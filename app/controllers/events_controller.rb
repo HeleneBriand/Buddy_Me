8634b05922bd378496c6095fbae28ec45bc88912
ines naamane <iness.naamane@gmail.com>
2022-11-21 22:49:13 +0100

    controllers-update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,12 @@
 class EventsController < ApplicationController
+  before_action :set_event, :only [:show, :destroy]
+  
+  def index
+    @events = Event.all
+  end
+
+  def show; end
+
   def new
     @event = Event.new
   end
@@ -10,5 +18,16 @@ class EventsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @event.destroy
+    # redirect_to buddies_path status: :see_other
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
