b0e7163b1df36d40fd02ff33bb484b7593b41a71
ines naamane <iness.naamane@gmail.com>
2022-11-21 18:16:55 +0100

    controller buddies actions

--- app/controllers/buddies_controller.rb
+++ app/controllers/buddies_controller.rb
@@ -1,16 +1,32 @@
 class BuddiesController < ApplicationController
+
+  def index
+    @buddies = Buddy.all
+  end
+
+  def show
+    @buddy = Buddy.find(params[:id])
+  end
+
   def new
+    @buddy = Buddy.new
   end
 
   def create
+    @buddy = Buddy.new(buddy_params)
+    if @buddy.save
+      redirect_to buddy_path(@buddy)
+    else
+      render :new
+    end
+
   end
 
   def destroy
   end
 
-  def index
-  end
+  def buddy_params
+    params.require(:user).permit(:description, :category)
 
-  def show
   end
 end
