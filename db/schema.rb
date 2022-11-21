2f85d0def6f75c62c71d21f4471a8add4c723b35
adriendavin <adriendavin@hotmail.fr>
2022-11-21 15:21:22 +0100

    creation model buddy

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_133336) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_141201) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "buddies", force: :cascade do |t|
+    t.text "description"
+    t.string "category"
+    t.string "city"
+    t.bigint "users_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["users_id"], name: "index_buddies_on_users_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +39,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_133336) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "buddies", "users", column: "users_id"
 end
