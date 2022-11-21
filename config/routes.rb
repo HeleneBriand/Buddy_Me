665c476b242717f68f776ffe8aacbca5e543d648
ines naamane <iness.naamane@gmail.com>
2022-11-21 21:48:53 +0100

    add action to events routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-    resources :buddies do
-      resources :events, only: [:new, :create]
-    end
-    resources :events, only: [:destroy]
+  resources :buddies do
+    resources :events, only: %i[index new create show]
+  end
+  resources :events, only: [:destroy]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
