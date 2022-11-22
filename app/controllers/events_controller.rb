51e834f8354641040f0854ff3b525ed126d20389
ines naamane <iness.naamane@gmail.com>
2022-11-22 11:07:44 +0100

    routes and controller update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,22 +1,21 @@
 class EventsController < ApplicationController
-  before_action :set_event, :only [:show, :destroy]
-  
+  before_action :set_event, only: [:show, :destroy]
+
   def index
     @events = Event.all
   end
 
   def show; end
 
-  def new
-    @event = Event.new
-  end
-
   def create
-    @event = Event.new(params[:user_id][:buddy_id])
-    if @event.save
-      redirect_to buddies_path(@buddies)
+    @event = Event.new
+    @buddy = Buddy.find(params[:buddy_id])
+    @event.user = current_user
+    @event.buddy = @buddy
+    if @event.save!
+      redirect_to buddies_path
     else
-      render :new, status: :unprocessable_entity
+      render buddy_path(@buddy), status: :unprocessable_entity
     end
   end
 
@@ -30,4 +29,5 @@ class EventsController < ApplicationController
   def set_event
     @event = Event.find(params[:id])
   end
+
 end
