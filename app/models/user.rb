a4162a2c2776be1cbb2538d2440adc8c84d09fc0
adriendavin <adriendavin@hotmail.fr>
2022-11-22 12:09:06 +0100

    validations de user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :sex, presence: true
 
 end
