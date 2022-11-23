d8990c4ff2059b36715ef5033fc3853d4a4751ff
adriendavin <adriendavin@hotmail.fr>
2022-11-23 13:01:20 +0100

    clean status

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,81 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'open-uri'
 puts "Destroying database"
 Buddy.destroy_all
 User.destroy_all
 
 puts "Creating users"
-user1 = User.create!(name: "Nony", age: 23, sex: "male", email: "Nony_bb@gmail.com", password: "Nono13")
+user0 = User.create!(name: "Nony", age: 23, sex: "male", email: "Nony_bb@gmail.com", password: "Nono13")
+user1 = User.create!(name: "mathilde", age: 25, sex: "female", email: "math@gmail.com", password: "mathilde")
 user2 = User.create!(name: "Intissar", age: 21, sex: "female", email: "inti@gmail.com", password: "bgdu13")
 user3 = User.create!(name: "Lucas", age: 26, sex: "male", email: "anonymous@gmail.com", password: "nonyloveu")
 user4 = User.create!(name: "Antoine", age: 18, sex: "male", email: "tonio@gmail.com", password: "petanqueloveur")
+user5 = User.create!(name: "Julien", age: 27, sex: "male", email: "juju@gmail.com", password: "juju13")
+user6 = User.create!(name: "Lucie", age: 30, sex: "female", email: "lulu@gmail.com", password: "lulu13")
+user7 = User.create!(name: "Joy", age: 23, sex: "female", email: "joie@gmail.com", password: "joy13003")
+user8 = User.create!(name: "Mia", age: 30, sex: "male", email: "mimi@gmail.com", password: "mimimi")
+user9 = User.create!(name: "Nicolas", age: 22, sex: "male", email: "nico@gmail.com", password: "nico11")
+user10 = User.create!(name: "Helene", age: 30, sex: "female", email: "heleneb@gmail.com", password: "heleneb")
+user11 = User.create!(name: "hermann", age: 17, sex: "male", email: "herman@gmail.com", password: "hermann")
+user12 = User.create!(name: "Adrien", age: 25, sex: "male", email: "adrien@gmail.com", password: "adrien")
 puts "Users finished"
 
+nony = URI.open("https://avatars.githubusercontent.com/u/111756663?v=4")
+mathilde = URI.open("https://avatars.githubusercontent.com/u/114300122?v=4")
+intissar = URI.open("https://avatars.githubusercontent.com/u/115433789?v=4")
+lucas = URI.open("https://avatars.githubusercontent.com/u/111721957?v=4")
+antoine = URI.open("https://avatars.githubusercontent.com/u/114173699?v=4")
+julien = URI.open("https://avatars.githubusercontent.com/u/112513305?v=4")
+lucie = URI.open("https://avatars.githubusercontent.com/u/112090726?v=4")
+joy = URI.open("https://avatars.githubusercontent.com/u/110812012?v=4")
+mia = URI.open("https://avatars.githubusercontent.com/u/115114104?v=4")
+nico = URI.open("https://avatars.githubusercontent.com/u/114221646?v=4")
+helene = URI.open("https://avatars.githubusercontent.com/u/23334343?v=4")
+hermann = URI.open("https://avatars.githubusercontent.com/u/63840698?v=4")
+adrien = URI.open("https://avatars.githubusercontent.com/u/107648853?v=4")
+
+
+
 puts "Creating buddies"
-Buddy.create!(user_id: user1.id, description: "i'am alone and i suffer i want friends please!", category: "all", city: "marseille")
-Buddy.create!(user_id: user2.id, description: "i like to participate to weddings", category: "celebrations", city: "nice")
-Buddy.create!(user_id: user3.id, description: "i'm a good listener", category: "workout", city: "paris")
-Buddy.create!(user_id: user4.id, description: "i like to play and drink a lot", category: "all", city: "grenoble")
+nony1 = Buddy.create!(user_id: user0.id, description: "i'am alone and i suffer i want friends please!", category: "all", city: "marseille")
+nony1.photo.attach(io: nony, filename: "nony.jpeg", content_type: "image/jpeg")
+
+mathilde1 = Buddy.create!(user_id: user1.id, description: "i'am alone and i suffer i want friends please!", category: "all", city: "marseille")
+mathilde1.photo.attach(io: mathilde, filename: "mathilde.jpeg", content_type: "image/jpeg")
+
+inti = Buddy.create!(user_id: user2.id, description: "i like to participate to weddings", category: "celebrations", city: "nice")
+inti.photo.attach(io: intissar, filename: "intissar.jpeg", content_type: "image/jpeg")
+
+lulu = Buddy.create!(user_id: user3.id, description: "i'm a good listener", category: "workout", city: "paris")
+lulu.photo.attach(io: lucas, filename: "lucas.jpeg", content_type: "image/jpeg")
+
+tonio = Buddy.create!(user_id: user4.id, description: "i like to play and drink a lot", category: "all", city: "marseille")
+tonio.photo.attach(io: antoine, filename: "antoine.jpeg", content_type: "image/jpeg")
+
+juju = Buddy.create!(user_id: user5.id, description: "i like to play and drink a lot", category: "all", city: "grenoble")
+juju.photo.attach(io: julien, filename: "julien.jpeg", content_type: "image/jpeg")
+
+lulucie = Buddy.create!(user_id: user6.id, description: "i like to play and drink a lot", category: "all", city: "amiens")
+lulucie.photo.attach(io: lucie, filename: "lucie.jpeg", content_type: "image/jpeg")
+
+joie = Buddy.create!(user_id: user7.id, description: "i like to play and drink a lot", category: "all", city: "grenoble")
+joie.photo.attach(io: joy, filename: "joy.jpeg", content_type: "image/jpeg")
+
+mimi = Buddy.create!(user_id: user8.id, description: "i like to play and drink a lot", category: "all", city: "nice")
+mimi.photo.attach(io: mia, filename: "mia.jpeg", content_type: "image/jpeg")
+
+nicolas = Buddy.create!(user_id: user9.id, description: "i like to play and drink a lot", category: "all", city: "toulon")
+nicolas.photo.attach(io: nico, filename: "nico.jpeg", content_type: "image/jpeg")
+
+helene1 = Buddy.create!(user_id: user10.id, description: "i like to play and drink a lot", category: "all", city: "paris")
+helene1.photo.attach(io: helene, filename: "helene.jpeg", content_type: "image/jpeg")
+
+hermann1 = Buddy.create!(user_id: user11.id, description: "i like to play and drink a lot", category: "all", city: "nimes")
+hermann1.photo.attach(io: hermann, filename: "hermann.jpeg", content_type: "image/jpeg")
+
+adri = Buddy.create!(user_id: user12.id, description: "i like to play and drink a lot", category: "all", city: "lyon")
+adri.photo.attach(io: adrien, filename: "adrien.png", content_type: "image/png")
+
 puts "Buddies finished"
