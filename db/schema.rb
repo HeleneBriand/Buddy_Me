8cdf312d4049c2b263704f6fe4d206d8569ffaff
adriendavin <adriendavin@hotmail.fr>
2022-11-23 12:46:14 +0100

    rav

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2022_11_22_181934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,11 +63,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_181934) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password", null: false
-    t.string "name"
-    t.string "age"
-    t.string "sex"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", default: ""
+    t.integer "age"
+    t.string "sex", default: ""
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
