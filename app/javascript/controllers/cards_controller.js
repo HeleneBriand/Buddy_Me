9335bcc82ff2e81eb810ef83b41433e72e54f6d8
ines naamane <iness.naamane@gmail.com>
2022-11-23 14:17:05 +0100

    js controller

--- app/javascript/controllers/cards_controller.js
+++ app/javascript/controllers/cards_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="cards"
 export default class extends Controller {
   connect() {
-    console.log("Hello from our first Stimulus controller")
+    console.log("Hello from our first Stimulus controller");
   }
 }
