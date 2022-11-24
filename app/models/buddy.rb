eb3bec1b463ad8710161b367f0f6e47facc9e52b
ines naamane <iness.naamane@gmail.com>
2022-11-24 14:35:53 +0100

    create new seeds, update events controller, update buddy model

--- app/models/buddy.rb
+++ app/models/buddy.rb
@@ -2,8 +2,7 @@ class Buddy < ApplicationRecord
   CATEGORY = ['all', 'celebrations', 'workout', 'work', 'love', 'holidays']
 
   belongs_to :user
-  has_many :events
-  has_one_attached :photo
+  has_many :events, dependent: :destroy
   validates :description, length: { minimum: 10 }, presence: true
   validates :category, presence: true, inclusion: { in: CATEGORY, message: '%{value} is not a valid category' }
   validates :city, presence: true
