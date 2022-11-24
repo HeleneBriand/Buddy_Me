5f4968a9763b61b922a706e3a57a711585bae21e
Mathilde Aubry <aubry.mathilde92@gmail.com>
2022-11-24 11:02:22 +0100

    menu déroulant sex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  SEX = ['F', 'M']
   has_many :buddies
   has_many :events
- 
+
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :sex, inclusion: { in: SEX, message: '%{value} is not a valid category' }
 end
