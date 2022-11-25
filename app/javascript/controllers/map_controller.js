dcb9ea3e143f57f975016ddda2b7469b2484cb49
Helene Briand <helenebriand01@gmail.com>
2022-11-25 12:36:00 +0100

    map + index

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/hebrd/clawfb8np004g14lg8sybaa1u"
     })
 
     this.#addMarkersToMap()
